85b7e85d405182111457cc5b5951e1243530db14
georgiaboy02 <tbrewer1@mail.mvnu.edu>
2015-05-10 01:13:10 -0400

    added intro buttion functionality

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
-	$(".intro").hide();
-	$("outro").hide();
 	$(".question-1").hide();
 	$(".question-2").hide();
 	$(".question-3").hide();
 	$(".question-4").hide();
+	$(".question-5").hide();
+	$(".outro").hide();
+
+	$(".start-button").click(function() {
+		$(".intro").hide()
+		$(".question-1").show();
+		return questionNum++;
+	})
 });
