5020e864fc51b62975f74bbd7ad6a5ccffec996b
georgiaboy02 <tbrewer1@mail.mvnu.edu>
2015-05-10 00:51:13 -0400

    reduced image size

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	$(".intro").hide();
+	$(".intro").hide();
 	$(".question-1").hide();
 	$(".question-2").hide();
 	$(".question-3").hide();
 	$(".question-4").hide();
-	$(".question-5").hide();	
 });
